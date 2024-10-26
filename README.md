d7c9038c8a9fbb2d1855d61ef5b7247b72899a79
emrd434 <emrd434@gmail.com>
2024-10-26 19:53:36 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NodeJS Firebase Remote Config Example
+
+This is a simple API sample in Node.js with express.js based on Firebase Remote Config.
+
+## Getting Started
+
+1 - Create a project in Firebase.
+
+2 - Add your web config to Firebase. 
+
+3 - Create Remote Config template and add some values.
+
+4 - Run ```npm i``` to install the dependencies. 
+
+5 - Login to Firebase with the CLI. 
+
+6 - Configure your project ID in ```index.js``` file.
+
+7 - To run server, run:
+```sh
+npm run dev
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+import { initializeApp, applicationDefault } from "firebase-admin/app";
+import { getRemoteConfig } from "firebase-admin/remote-config";
+import express from "express";
+const app = express();
+
+initializeApp({
+  credential: applicationDefault(),
+  projectId: "YOUR_PROJECT_ID",
+});
+
+async function getWelcomeMessage() {
+  try {
+    const rc = getRemoteConfig();
+    const template = await rc.getTemplate(); 
+    const welcomeMessage = template.parameters?.welcome_message?.defaultValue?.value || "Default Welcome Message";
+    return welcomeMessage;
+  } catch (error) {
+    return "Error fetching welcome message" + error;
+  }
+}
+
+app.get("/" , (req , res) => {
+    getWelcomeMessage().then((message) => {
+      res.send(message);
+    });
+  })
+
+app.listen(3000, () => {
+  console.log(`Server is running on port http://localhost:3000`);
+});
